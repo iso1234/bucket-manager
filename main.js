4984066c95fe00ece0da1ea3e1a6336365acf2d2
Isaac <greeneisaac@inaburrastudents.nsw.edu.au>
2016-08-13 21:02:29 +1000

    Master

---
+++ main.js
@@ -0,0 +1,108 @@
+/* My modification of the node.js sample file from the AWS website */
+
+
+/*** Include dependencies ***/
+
+// Load the SDK and UUID
+var AWS = require('aws-sdk');
+// UUID(universally unique IDs)
+var uuid = require('node-uuid');
+// jsonfile is used to write/read json files
+var jsonfile = require('jsonfile');
+
+
+// Tested
+/* ------------------------------------------ Create an empty bucket -------------------------------------------- */
+
+function createBucket(bktName, btkReason) {
+    // Create an S3 client
+    var s3 = new AWS.S3();
+    
+    // Create an empty bucket
+    var bucketName = bktName + uuid.v4();
+    s3.createBucket({Bucket: bucketName}, function () {});
+    
+    // Return the entire bucket name
+    return bucketName;
+}
+/* ------------------------------------------------------------------------------------------------------------------------- */
+
+
+
+/* --------------------------------------------- Deletes a bucket -------------------------------------------------- */
+
+
+
+
+/* -------------------------------------------------------------------------------------------------------------------------- */
+
+
+// Needs testing
+/* -------------------------------------------- List all of my buckets --------------------------------------------- */
+
+function getBucketList() {
+    // Create an S3 client
+    var s3 = new AWS.S3();
+    
+    var bucketArray = []
+    
+    s3.listBuckets(function(err, data) {
+        if (err) { 
+            console.error(err);
+        } else {
+            for (var index in data.Buckets) {
+                var bucket =  data.Buckets[index];
+                bucketArray.push(bucket)
+                //console.log("Bucket: ", bucket.Name, ' : ', bucket.CreationDate);
+            }
+        }
+    });
+    return bucketArray
+}
+/* -------------------------------------------------------------------------------------------------------------------------- */
+
+
+
+// Tested
+/* ------------------- Log the purpose of the bucket to the designated file ---------------------------- */
+
+function logBucketData(bktName, bktPurpose) {
+    // Define the purpose of the bucket, this will be logged in a file in /Users/Isaac/AWS
+    var bucketPurpose = bktPurpose;
+    var bucketName = bktName;
+    
+    // Log the name of the bucket and the purpose to a file
+    var fName = "/Users/Isaac/AWS/bucketInfo.json";
+    var fileData = {};
+    
+    
+    // Get the data from the file
+    jsonfile.readFile(fName, function(err, obj) {
+        if (err) {
+            console.error(err);
+        } else {
+            fileData = obj;
+            // Add a new property tot the object
+            fileData[bucketName] = bktPurpose;
+            saveData();
+        }
+    });
+    
+    // Save the data
+    function saveData() {
+        jsonfile.writeFile(fName, fileData, {spaces: 4}, function(err) {
+            if (err) {
+                console.error(err);
+            }
+        });
+    }
+}
+/*----------------------------------------------------------------------------------------------------------------------------*/
+
+
+function Main() {
+    var fullBucketName = createBucket("MFPLiveBucket");
+    logBucketData(fullBucketName, "Live bucket for the project: MyFirstProject")
+}
+
+Main();
